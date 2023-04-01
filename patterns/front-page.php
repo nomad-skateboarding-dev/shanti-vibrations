c0fd80384595c522135d1d36daae57bbb72e793e
Nomad Skateboarding <78917704+nomad-skateboarding-dev@users.noreply.github.com>
2023-04-01 12:29:26 -0700

    Add files via upload

---
+++ patterns/front-page.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: shantivibrations/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/images-1.jpeg","id":86,"hasParallax":true,"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","contentPosition":"top left","align":"wide"} -->
+<div class="wp-block-cover alignwide has-parallax has-custom-content-position is-position-top-left" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background wp-image-86 has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/images-1.jpeg)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|80","top":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--80)"><!-- wp:site-title {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"fontSize":"large"} /-->
+
+<!-- wp:navigation {"ref":18,"textColor":"secondary","overlayMenu":"always","icon":"menu","overlayBackgroundColor":"secondary","overlayTextColor":"background","layout":{"type":"flex","justifyContent":"center","orientation":"horizontal","flexWrap":"nowrap"},"fontSize":"huge"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"radius":"16px"},"spacing":{"padding":{"top":"5px","right":"5px","bottom":"5px","left":"5px"}}},"backgroundColor":"secondary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-secondary-background-color has-background" style="border-radius:16px;padding-top:5px;padding-right:5px;padding-bottom:5px;padding-left:5px"><!-- wp:heading {"textAlign":"center","level":1,"textColor":"tertiary","className":"wp-block-heading"} -->
+<h1 class="wp-block-heading has-text-align-center has-tertiary-color has-text-color">I'm Shanti 🧙‍♂️</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"textAlign":"left","style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}},"textColor":"foreground","className":"wp-block-heading"} -->
+<h2 class="wp-block-heading has-text-align-left has-foreground-color has-text-color" style="margin-top:var(--wp--preset--spacing--80)"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-secondary-color">Art</mark> and <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-tertiary-color">Music</mark> is the way I intuitively flow and connect with <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-quaternary-color">Spirit</mark></h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"secondary","width":50,"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-outline"><a class="wp-block-button__link has-secondary-color has-text-color wp-element-button" href="https://shantivibrationsnsb.art/shop/">Shop</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"textColor":"quaternary","width":50,"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-outline"><a class="wp-block-button__link has-quaternary-color has-text-color wp-element-button" href="https://shantivibrationsnsb.art/contact/">Contact</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"0","top":"var:preset|spacing|80"}}},"gradient":"foreground-to-secondary","className":"is-style-shantivibrationsnsb-angled-separator"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity has-foreground-to-secondary-gradient-background has-background is-style-shantivibrationsnsb-angled-separator" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:0"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"white","fontSize":"tiny"} -->
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-tiny-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">👇 Scroll To Explore 👇</h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"0","top":"0"}}},"gradient":"foreground-to-secondary","className":"is-style-shantivibrationsnsb-angled-separator"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity has-foreground-to-secondary-gradient-background has-background is-style-shantivibrationsnsb-angled-separator" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/images-1.jpeg","id":86,"hasParallax":true,"dimRatio":0,"minHeight":2801,"minHeightUnit":"px","align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-cover alignfull has-parallax" style="margin-top:0;margin-bottom:0;min-height:2801px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background wp-image-86 has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/images-1.jpeg)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"border":{"radius":"50px","top":{"color":"var:preset|color|primary","width":"1px"},"right":{"width":"1px","radius":"54px","color":"var:preset|color|quaternary"},"bottom":{"color":"var:preset|color|pale-pink","width":"1px"},"left":{"color":"var:preset|color|secondary","width":"1px"}},"spacing":{"blockGap":"1.5rem"}},"textColor":"foreground","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group has-foreground-color has-text-color" style="border-radius:50px;border-top-color:var(--wp--preset--color--primary);border-top-width:1px;border-right-color:var(--wp--preset--color--quaternary);border-right-width:1px;border-bottom-color:var(--wp--preset--color--pale-pink);border-bottom-width:1px;border-left-color:var(--wp--preset--color--secondary);border-left-width:1px"><!-- wp:spacer {"height":"39px"} -->
+<div style="height:39px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"className":"wp-block-heading"} -->
+<h2 class="wp-block-heading has-text-align-center has-link-color"><a href="https://shantivibrationsnsb.art/shop/" data-type="page" data-id="5">Jewelry • Sculptures • Energy Readings</a></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"wp-block-heading","fontSize":"extra-large"} -->
+<p class="has-text-align-center wp-block-heading has-extra-large-font-size">I love to explore creative outlets and share the intentions I imbue in each with the world and you ❤️</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--80)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":242,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"50px"}},"borderColor":"quaternary","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-large has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Screenshot_20221015-154231-780x1024.png" alt="" class="has-border-color has-quaternary-border-color wp-image-242" style="border-radius:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:separator {"gradient":"quaternary-to-background","className":"is-style-shantivibrationsnsb-angled-separator-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity has-quaternary-to-background-gradient-background has-background is-style-shantivibrationsnsb-angled-separator-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"style":{"border":{"radius":"50px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"borderColor":"quaternary","textColor":"quaternary"} -->
+<div class="wp-block-group has-border-color has-quaternary-border-color has-quaternary-color has-text-color" style="border-radius:50px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"textAlign":"left","style":{"typography":{"lineHeight":"1"}},"className":"wp-block-heading","fontSize":"extra-large","anchor":"my-story"} -->
+<h2 class="wp-block-heading has-text-align-left has-extra-large-font-size" id="my-story" style="line-height:1">Copper and Silver Handmade Jewelry</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size">Tap into my self-expression through  handmade jewelry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"quaternary","className":"is-style-fill","fontSize":"tiny"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill has-tiny-font-size"><a class="wp-block-button__link has-quaternary-background-color has-background wp-element-button" href="https://shantivibrationsnsb.art/product-category/jewelry/">Shop Jewelry</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"textColor":"primary"} -->
+<div class="wp-block-column has-primary-color has-text-color"><!-- wp:image {"id":237,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"50px"}},"borderColor":"primary","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Screenshot_20221013-172530-662x1024.jpg" alt="" class="has-border-color has-primary-border-color wp-image-237" style="border-radius:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:separator {"gradient":"primary-to-background","className":"is-style-shantivibrationsnsb-angled-separator-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity has-primary-to-background-gradient-background has-background is-style-shantivibrationsnsb-angled-separator-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"style":{"border":{"radius":"50px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"borderColor":"primary"} -->
+<div class="wp-block-group has-border-color has-primary-border-color" style="border-radius:50px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"textAlign":"left","style":{"typography":{"lineHeight":"1"}},"className":"wp-block-heading","fontSize":"extra-large","anchor":"my-story"} -->
+<h2 class="wp-block-heading has-text-align-left has-extra-large-font-size" id="my-story" style="line-height:1">Sculptures</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size">Want to show my art in your gallery? Send me an email and
+				we'll chat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"primary","textColor":"background","fontSize":"tiny"} -->
+<div class="wp-block-button has-custom-font-size has-tiny-font-size"><a class="wp-block-button__link has-background-color has-primary-background-color has-text-color has-background wp-element-button" href="https://shantivibrationsnsb.art/product-category/sculptures/">Shop Sculptures</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"textColor":"tertiary"} -->
+<div class="wp-block-column has-tertiary-color has-text-color"><!-- wp:image {"id":575,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"50px"}},"borderColor":"foreground","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/IMG_20221109_153651-823x1024.jpg" alt="" class="has-border-color has-foreground-border-color wp-image-575" style="border-radius:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:separator {"gradient":"foreground-to-background","className":"is-style-shantivibrationsnsb-angled-separator-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity has-foreground-to-background-gradient-background has-background is-style-shantivibrationsnsb-angled-separator-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"style":{"border":{"radius":"50px"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"borderColor":"foreground"} -->
+<div class="wp-block-group has-border-color has-foreground-border-color" style="border-radius:50px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"textAlign":"left","style":{"typography":{"lineHeight":"1"}},"textColor":"foreground","className":"wp-block-heading","fontSize":"extra-large","anchor":"my-story"} -->
+<h2 class="wp-block-heading has-text-align-left has-foreground-color has-text-color has-extra-large-font-size" id="my-story" style="line-height:1">Energy Readings</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","textColor":"foreground","fontSize":"small"} -->
+<p class="has-text-align-left has-foreground-color has-text-color has-small-font-size">Book a session and we'll schedule a time.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"foreground","fontSize":"tiny"} -->
+<div class="wp-block-button has-custom-font-size has-tiny-font-size"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" href="https://shantivibrationsnsb.art/product-category/energy-readings/">Book Sessions</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"backgroundColor":"secondary","textColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background-color has-secondary-background-color has-text-color has-background" style="margin-top:0"><!-- wp:template-part {"slug":"new","theme":"shantivibrationsnsb"} /--></div>
+<!-- /wp:group -->
---
+++ patterns/general-demo-content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Demo content for the theme, and default content for the Home template.
+ * Slug: shantivibrationsnsbnsb/general-demo-content
+ * Categories: shantivibrationsnsbnsb-general
+ */
+?>
+<!-- wp:pattern {"slug":"shantivibrationsnsbnsb/general-hero-full-height"} /-->
+
+<!-- wp:separator {"backgroundColor":"tertiary","className":"is-style-shantivibrationsnsbnsb-angled-separator-wide"} -->
+<hr class="wp-block-separator has-text-color has-tertiary-color has-alpha-channel-opacity has-tertiary-background-color has-background is-style-shantivibrationsnsbnsb-angled-separator-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:pattern {"slug":"shantivibrationsnsbnsb/general-columns-with-paragraphs"} /-->
+<!-- wp:pattern {"slug":"shantivibrationsnsbnsb/general-featured-items"} /-->
